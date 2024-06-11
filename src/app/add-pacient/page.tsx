d2f1cddcc8cf1811a130b66a1f35d87e18c08d67
Cauã Tavares <tavarescauac@gmail.com>
2024-06-11 17:18:43 -0300

    🎉 first commit

---
+++ src/app/add-pacient/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function page() {
+  return (
+    <main className="flex min-h-screen flex-col p-24">
+      {" "}
+      <h1>Pagina de paciente</h1>
+    </main>
+  );
+}
+
+export default page;
---
+++ src/app/list-pacients/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>lista de pacientes</div>
+    </main>
+  );
+}
+
+export default page;
