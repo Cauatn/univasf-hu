e797548c809937c0cd0401fa4787d9f9eea9c223
Cauã Tavares <tavarescauac@gmail.com>
2024-06-18 11:46:23 -0300

    refactor: Update individual patient page layout

--- src/app/list-pacients/[id]/page.tsx
+++ src/app/list-pacients/[id]/page.tsx
@@ -1,36 +1,37 @@
-"use client";
-
-import React from "react";
+import React, { Suspense } from "react";
 import { usePathname } from "next/navigation";
 import prisma from "@/db";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 
-async function page() {
-  const id = usePathname().replace("/list-pacients/", "").replace("/", "");
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
 
-  const pacient = await prisma.pacient.findUnique({
+async function page({ params }: PageProps) {
+  const id = params.id;
+
+  const paciente = await prisma.pacient.findUnique({
     where: {
       id: id,
     },
   });
 
-  if (!pacient) {
-    return <div>User not Found</div>;
-  }
-
   return (
-    <div>
-      {!pacient ? (
-        <div>loading...</div>
-      ) : (
+    <Suspense>
+      <div className="flex space-x-2">
+        <Avatar className="size-16">
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>X</AvatarFallback>
+        </Avatar>
         <div>
-          <h1>{pacient.name}</h1>
-          <p>{pacient.phone}</p>
-          <p>{pacient.room}</p>
-          <p>{pacient.treatment}</p>
-          <p>{pacient.status}</p>
+          <h1 className="text-2xl font-bold">{paciente?.name}</h1>
+          <Badge className="bg-green-400">{paciente?.status}</Badge>
         </div>
-      )}
-    </div>
+      </div>
+    </Suspense>
   );
 }
 
